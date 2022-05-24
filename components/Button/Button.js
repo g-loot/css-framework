0a9c633ecd9ab9463ebf1224a6b0c0aed9192f0b
Augustin Hiebel <augustin.hiebel@gmail.com>
2022-05-24 11:17:54 +0200

    Framework progress

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
   const icon = props.icon && (
-    <span className={`icon icon-${props.icon} ${props.iconSize ? `icon-${props.iconSize}` : ''} icon-${props.iconSize} ${props.iconDirection == 'right' ? 'order-2' : ''}`}></span>
+    <span className={`icon icon-${props.icon} ${props.iconSize ? `icon-${props.iconSize}` : ''} icon-${props.iconSize}`}></span>
   );
   return (
   <button className={`button button-${props.variant}`}>
