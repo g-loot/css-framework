8a603be722410be328506d8a9ef32d3f3c1dc0e2
Augustin Hiebel <augustin.hiebel@gmail.com>
2022-07-05 13:12:55 +0200

    npmrc file added

--- components/MainNav/MainNav.js
+++ components/MainNav/MainNav.js
@@ -16,14 +16,18 @@ export default function MainNav(props) {
   return (
     <ul className={`menu ${props.rounded === 'true' ? 'menu-rounded' : ''}  ${props.style === 'secondary' ? 'menu-secondary' : ''}  ${extraClassNames}`}>
       {props.items.map((item, itemIndex) => (
-        <li className={`${item.disabled === true ? 'is-disabled' : ''}`} key={itemIndex} onClick={props.onClick}>
-          <Link href={`${item.url}${hasAds ? '?ads=true' : ''}`} >
-            <a className={`${router.pathname.endsWith('prototype') && item.isHome ? 'is-active' : ''} ${router.pathname.includes(item.url) || router.pathname === '' ? 'is-active' : ''}`}>
-              <span className={`icon icon-16 icon-${item.icon}`}></span>
-              <span className='uppercase'>{item.label}</span>
-            </a>
-          </Link>
-        </li>
+        <>
+          <li className={`${item.disabled === true ? 'is-disabled' : ''}`} key={itemIndex} onClick={props.onClick}>
+            <Link href={`${item.url}${hasAds ? '?ads=true' : ''}`} >
+              <a className={`${router.pathname.endsWith('prototype') && item.isHome ? 'is-active' : ''} ${router.pathname.includes(item.url) || router.pathname === '' ? 'is-active' : ''}`}>
+                <span className="icon rounded w-8 flex items-center justify-center">
+                  <span className={`icon icon-16 icon-${item.icon}`}></span>
+                </span>
+                <span className='uppercase'>{item.label}</span>
+              </a>
+            </Link>
+          </li>
+        </>
       ))}
     </ul>
   );
--- components/Prototype/PrototypeStructure.js
+++ components/Prototype/PrototypeStructure.js
@@ -17,7 +17,7 @@ export default function PrototypeStructure({children, title}) {
       <Topbar />
       <div className="container flex mx-auto md:gap-12 md:px-4 2xl:px-0">
         <div className="hidden md:block top-16 w-auto md:w-3/12 xl:w-2/12 py-4">
-          <div className='sticky top-[68px] space-y-4'>
+          <div className='sticky top-[68px] space-y-2'>
             <div className="surface rounded-lg overflow-hidden">
               <MainNav items={JSONstructure.pages} />
             </div>
