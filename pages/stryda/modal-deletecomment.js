384dd0134dbf7b2628a7301828e3bb177691a0eb
Augustin Hiebel <augustin.hiebel@gmail.com>
2023-09-22 15:51:47 +0200

    Prototype: Stryda v2 progress

--- pages/stryda/modal-deletecomment.js
+++ pages/stryda/modal-deletecomment.js
@@ -1,15 +1,9 @@
 import React, { useContext, useState } from "react";
 
-import InputNumber from "@/components/InputNumber/InputNumber.js";
 import { UiContext } from "@/contexts/ui.js";
-import { usePrototypeData } from "@/contexts/prototype.js";
-import Avatar from "@/components/Avatar/Avatar";
-import FeedItemMatchTabSummary from "./components/FeedItemMatchTabSummary";
 
-export default function ModalDeleteComment(props) {
+export default function ModalDeleteComment() {
   const uiContext = useContext(UiContext);
-  const prototype = usePrototypeData();
-  const item = props.item;
   const [submitting, setSubmitting] = useState(false);
   function closeModalWithDelay() {
     setSubmitting(true);
@@ -29,15 +23,11 @@ export default function ModalDeleteComment(props) {
 
   return (
     <>
-    {match && (
       <div className="modal max-w-sm modal-center">
         <div className="modal-content">
           <div className="modal-body">
             <h2 className="modal-title h4">Delete comment?</h2>
-            <p>
-              You are about to delete the comment. Are you sure?
-            </p>
-
+            <p>You are about to delete the comment. Are you sure?</p>
           </div>
           <div className="modal-action">
             <button
@@ -59,7 +49,6 @@ export default function ModalDeleteComment(props) {
           </div>
         </div>
       </div>
-    )}
     </>
   );
 }
