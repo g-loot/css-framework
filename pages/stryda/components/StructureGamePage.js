c6274e9b5e3593adebdee24167654fb13b36c162
Augustin Hiebel <augustin.hiebel@gmail.com>
2023-10-12 16:57:28 +0200

    Prototype: Structure GameNav progress

--- pages/stryda/components/StructureGamePage.js
+++ pages/stryda/components/StructureGamePage.js
@@ -5,9 +5,10 @@ import GamePicker from "../components/GamePicker";
 import ButtonStatsUpdate from "@/components/ButtonStatsUpdate/ButtonStatsUpdate";
 
 export default function StructureGamePage({ children, side, ...props }) {
-  const prototype = usePrototypeData(); 
+  const prototype = usePrototypeData();
   const [loading, setLoading] = useState(true);
-  const title = props.title || "";
+  const title = props.title || "";
+  const selectedGame = prototype.getGameByID(prototype.defaultGameID);
 
   useEffect(() => {
     if (loading) {
@@ -26,8 +27,8 @@ export default function StructureGamePage({ children, side, ...props }) {
 
   return (
     <>
-      <div className="relative flex flex-col md:flex-row md:items-end md:justify-between md:gap-4 md:mb-8">
-        <div className="order-2 md:order-1 flex items-center justify-between px-4 md:px-0 mb-6 mt-8 md:mb-2 md:mt-16">
+      <div className="relative z-20 flex flex-col md:flex-row md:items-end md:justify-between md:gap-4 md:mb-8">
+        <div className="order-2 md:order-1 flex items-center justify-between px-4 md:px-0 mb-6 mt-8 md:mb-2 md:mt-24">
           <h1 className="text-7xl">{title}</h1>
           <div className="block md:hidden">
             <ButtonStatsUpdate hasTooltip={false} />
@@ -35,7 +36,16 @@ export default function StructureGamePage({ children, side, ...props }) {
         </div>
         <GamePicker />
       </div>
-      <div className="flex flex-col lg:flex-row lg:items-stretch gap-8">
+      {selectedGame && !loading && (
+        <div className="absolute z-0 -top-28 inset-x-0 animate-slide-in-bottom mix-blend-lighten"  style={{ animationDuration: "2s"}}>
+          <img
+            src={selectedGame.assets.heroV2}
+            alt={selectedGame.name}
+            className="opacity-75"
+          />
+        </div>
+      )}
+      <div className="relative z-20 flex flex-col lg:flex-row lg:items-stretch gap-8">
         <div className="w-full lg:w-56 lg:mb-8">
           <div className="lg:sticky top-16">{side}</div>
         </div>
